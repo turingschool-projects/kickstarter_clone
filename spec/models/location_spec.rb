a2672c08cd72d0c417182997c01f66567c0554ba
Thomas Soucie <tsoucie1858@gmail.com>
2017-07-27 10:25:48 -0600

    Ping Pong refactor
    - remove duplicate and umused model specs (country and city)
    - remove oountry relations in spec and factory and replace with :location
    - comment maps logic

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "associations" do
+    it { should have_many(:projects) }
+  end
 end

785533015d3e24b978ad233cfe9309a2a689e659
Benjamin Ross <benjaminross1001@gmail.com>
2017-08-01 17:22:14 -0600

    adds biggest donor business intelligence

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,12 @@ class User < ApplicationRecord
   has_many :project_backers
   has_many :backed_projects, through: :project_backers, source: :project
   has_many :rewards, through: :project_backers
+
+  def biggest_donor
+    User.select("users.*, sum(project_backers.pledge_amount) AS total_pledged")
+    .joins(:project_backers)
+    .group(:id)
+    .order("total_pledged DESC")
+    .limit(1)
+  end
 end
